20a48a91674a30520f8bb88403890a93a374bf1e
GastonOrquera <gastonorquera29@gmail.com>
2024-02-14 05:43:27 -0300

    Para-Mi-Amor

---
+++ main.js
@@ -0,0 +1,43 @@
+
+let bgOne = document.querySelector('.bgOne');
+
+let bgTwo = document.querySelector('.bgTwo');
+
+let bgThree = document.querySelector('.bgThree');
+
+let iOne = document.querySelector('.iOne');
+
+let iTwo = document.querySelector('.iTwo')
+
+
+iOne.addEventListener('click', function () {
+    bgOne.classList.toggle('active');
+    bgTwo.classList.toggle('active');
+
+});
+
+iTwo.addEventListener('click', function () {
+
+    bgThree.classList.toggle('active');
+
+});
+
+
+ScrollReveal().reveal('.scrollTitles', {
+    origin: 'left',
+    distance: '40px',
+    duration: 1000,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 200
+
+});
+
+ScrollReveal().reveal('.scroll-span', {
+    origin: 'right',
+    distance: '40px',
+    duration: 2000,
+    easing: 'cubic-bezier(0.5,0,0,1.4)',
+    interval: 200
+
+});
+
